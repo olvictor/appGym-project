7b29bdc9ef90c4a37a210b36381f22ddbf00f852
Victor olv <victorolvdev@gmail.com>
2023-05-18 22:28:56 -0300

    user att

--- src/Components/Pages/User.js
+++ src/Components/Pages/User.js
@@ -10,7 +10,15 @@ const User = () => {
   const {data} = useContext(UserContext);
 
   const [settings,setSettings] = useState(null);
-  const Height = UseForm()
+  const age = UseForm()
+  const height = UseForm()
+  const peso = UseForm()
+
+  const [kcal, setKcal] = useState('');
+  const [proteina, setProteina] = useState('');
+  const [carbo, setCarbo] = useState('');
+  const [gordura, setGordura] = useState('');
+
 
 
   return <section className={styles.section}>
@@ -22,13 +30,19 @@ const User = () => {
             repeat={0}
             style={{ fontSize: "1.4em", display: "inline-block", color: "#fff" }}
           />
-    {/* <h2>Hi, {data.nome} Here you can enter your information </h2> */}
     <hr />
-        <Input type='text' placeholder='Height'/>
-        <Input type='text' placeholder='Peso'/>
-        <Input type='text' placeholder='Idade'/>
+        <Input type='text' placeholder='Height'{...height}/>
+        <Input type='number' placeholder='Peso' {...peso}/>
+        <Input type='number' placeholder='Idade' {...age}/>
       <button>Register info</button>  
     </form>
+
+    <div className={styles.userInfo}>
+        <input type='text'  placeholder={`Nome: ${data.nome}`} disabled/>
+        <input type='text'  placeholder={`Altura: ${height.value} m`} disabled/>
+        <input type='text'  placeholder={`Idade: ${age.value} Anos`} disabled/>
+        <input type='text'  placeholder={`Peso: ${peso.value} kg`} disabled/>
+    </div>
   </section>;
 };
 
