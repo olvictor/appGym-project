3ed2dfd0fe6f74f9b8b4e35dbbffbc15b9f2cd29
Victorolv <victorolvdev@gmail.com>
2023-07-19 20:09:33 -0300

    galery update

--- src/Components/Pages/Galery.js
+++ src/Components/Pages/Galery.js
@@ -16,8 +16,9 @@ const Galery = () => {
   const [photos,setPhotos] = useState([]);
   const [images, setImages] = useState(null);
   const [activeIndex, setActiveIndex] = useState(0);    
+  const [activeShow, setActiveShow] = useState(false)
   const galleria = useRef(null);
-
+  const icon = <i className="pi pi-check"></i>
 
 const itemTemplate = (item) => {
     return <img src={item.src} alt={item.title} style={{ width: '100%', display: 'block' }} />;
@@ -45,21 +46,19 @@ useEffect(()=>{
   return <section className={`${styles.Galery} animeRigth`}>
   
     <div className="card flex justify-content-center">
-            <Galleria ref={galleria} value={photos} numVisible={7} style={{ maxWidth: '850px' }}
+            <Galleria ref={galleria} showIndicators={true} value={photos} numVisible={7} style={{ maxWidth: '850px' }}
             activeIndex={activeIndex} onItemChange={(e) => setActiveIndex(e.index)}
             circular fullScreen showItemNavigators showThumbnails={false} item={itemTemplate} thumbnail={thumbnailTemplate} />
             <div style={{ maxWidth: '100%', maxHeight:'auto', display:'flex', gap:'10px',marginTop:'50px',flexWrap:'wrap' }}>
                 {
                     images && images.map((image, index) => {
-                        let imgEl = <img src={image.src}  alt={image.title} style={{ cursor: 'pointer' , width:'250px' , height:'300px',borderRadius:'10px'}} onClick={
+                        let imgEl = <Image onMouseMove={()=> setActiveShow(index)} onMouseLeave={()=> setActiveShow(false)} imageStyle={{height:'300px', width:'250px',cursor: 'pointer'}} src={image.src}  alt={image.title} onClick={
                             () => {setActiveIndex(index); galleria.current.show()}
                         } />
                         return (
                             <div className={styles.Galery} style={{position:'relative'}} key={index}>
                                 {imgEl}
-                                <div className="teste">
-                                  <i style={ { position:'absolute',left:'0' ,backgroundColor:'red'}}  className="pi pi-check"></i>
-                                </div>
+                               <i style={ { display: activeShow === index ? 'flex' :'none', position:'absolute',left:'115px' ,top:'125px',color:'white',fontSize: '1.6rem' }}  className="pi pi-eye"></i>
                             </div>
                         )
                     })
